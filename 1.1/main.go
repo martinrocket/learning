4d676cc9b227480f88cf89e7d7a51caa273ec70f
martinrocket <martin.ruble@gmx.com>
2019-05-18 11:20:10 -0600

    small docker container build

--- 1.1/main.go
+++ 1.1/main.go
@@ -3,9 +3,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+   	"os/exec"
 )
 
 func main(){
-	fmt.Printf("Lesson 1.\nStarting....\n")
+	c := exec.Command("clear")
+   	c.Stdout = os.Stdout
+   	c.Run()
+	fmt.Printf("Lesson 1.\nStarting....\n...started\n")
 }
 
